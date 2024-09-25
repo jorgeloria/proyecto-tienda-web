230de32202cc251864f924f36d9db9cf9ebf02f9
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-09-25 17:05:11 -0600

    Change navbar and navbar color. Add menu items for mobile

--- DracoTech/src/components/Navbar.jsx
+++ DracoTech/src/components/Navbar.jsx
@@ -3,85 +3,132 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">daisyUI</Link>
+<div className="navbar bg-Footer_color">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
       </div>
-      <div className="flex-none">
-        <div className="menu menu-horizontal px-1">
-          <Link to="/" className="btn btn-ghost">Home</Link>
-          <Link to="/Login" className="btn btn-ghost">Login</Link>
-          <Link to="/Category" className="btn btn-ghost">Category</Link>
-          <Link to="/ShoppingCart" className="btn btn-ghost">ShoppingCart</Link>
-        </div>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Avatar de usuario */}
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-Footer_color rounded-box z-[1] mt-3 w-52 p-2 shadow [&>*]:p-1">
+        <li>
+          <details>
+            <summary>LAPTOPS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Laptops Nuevas</a></li>
+              <li><a>Laptops Usadas</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>COMPUTADORAS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Hogar / Oficina</a></li>
+              <li><a>Gaming</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>COMPONENTES</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Procesadores</a></li>
+              <li><a>Tarjetas Madre</a></li>
+              <li><a>RAM</a></li>
+              <li><a>Tarjetas Gráficas</a></li>
+              <li><a>Almacenamiento</a></li>
+              <li><a>Torres</a></li>
+              <li><a>PSUs</a></li>
+              <li><a>Enfriamiento</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>PERIFÉFICOS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Teclados</a></li>
+              <li><a>Mouse</a></li>
+              <li><a>Headset</a></li>
+              <li><a>Parlantes</a></li>
+              <li><a>Micrófonos</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>CELULARES</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Celulares Nuevos</a></li>
+              <li><a>Celulares Usados</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>TABLETS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Tablets Nuevas</a></li>
+              <li><a>Tablets Usadas</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>ACCESORIOS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Cables</a></li>
+              <li><a>Adaptadores</a></li>
+              <li><a>Extensiones</a></li>
+              <li><a>Redes</a></li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>CONSOLAS</summary>
+            <ul className="p-2 [&>*]:p-1">
+              <li><a>Consolas Nuevas</a></li>
+              <li><a>Consolas Usadas </a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
   );
 };
 
