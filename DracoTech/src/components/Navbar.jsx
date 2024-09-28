600961503afd807339903cd28930a604059cafa8
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-09-28 15:03:15 -0600

    Finish navbar

--- DracoTech/src/components/Navbar.jsx
+++ DracoTech/src/components/Navbar.jsx
@@ -2,26 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/logo_primary_color-transparent.png"
 import "../styles/Navbar.css"
+import CategoryItem from "./Navbar/CategoryItem";
+import SidebarCategoryItem from "./Navbar/SidebarCategoryItem";
+import SubCategoryItem from "./Navbar/SubCategoryItem";
 
 
 const Navbar = () => {
-
-  // Toggle mobile navbar menu
-  const toggleVisibility = () => {
-    let dropdownMenu = document.getElementById("navbar-dropdown-menu")
-    if (dropdownMenu.style.display == "none") {
-      dropdownMenu.style.display = "block";
-    } else {
-      dropdownMenu.style.display = "none";
-    }
-  }
-
   return (
     <div className="drawer">
       <input id="navbar-mobile-menu" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Navbar */}
-        <div className="navbar flex bg-Footer_color justify-between">
+        <div className="navbar flex bg-Footer_color justify-content-between">
           {/* Category Button (for mobile) */}
           <label htmlFor="navbar-mobile-menu" tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
@@ -37,48 +29,149 @@ const Navbar = () => {
             </svg>
           </label>
           {/* Navbar Logo */}
-          <div className="avatar w-16 mx-5 rounded">
+          <div className="w-16 mx-5 rounded" role="button">
               <img src="/src/images/logo_primary_color-transparent.png" alt=""/>
           </div>
           {/* Navbar Categories */}
-          <div className="navbar-categories hidden lg:block w-full mx-10">
-            <ul className="menu menu-horizontal flex justify-evenly">
-              <li>
-                <a className="dropdown dropdown-hover dropdown-bottom dropdown-center">
-                  <strong>LAPTOPS</strong>
-                  <ul className="dropdown-content menu bg-Footer_color rounded-box w-52">
-                    <li><a>Laptops Nuevas</a></li>
-                    <li><a>Laptops Usadas</a></li>
-                  </ul>
-                </a>
-              </li>
-              <li><a><strong>COMPUTADORAS</strong></a></li>
-              <li><a><strong>COMPONENTES</strong></a></li>
-              <li><a><strong>PERIFERICOS</strong></a></li>
-              <li><a><strong>CELULARES</strong></a></li>
-              <li><a><strong>TABLETS</strong></a></li>
-              <li><a><strong>ACCESORIOS</strong></a></li>
-              <li><a><strong>CONSOLAS</strong></a></li>
+          <div className="navbar-categories lg:block w-full mx-10">
+            <ul className="navbar-category-list menu menu-horizontal invisible lg:visible flex lg:justify-evenly">
+              <CategoryItem categoryName={"LAPTOPS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Laptops Nuevas"} />,
+                  <SubCategoryItem subCategoryName={"Laptops Usadas"} />
+                ]}
+              />
+              <CategoryItem categoryName={"COMPUTADORAS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Hogas / Oficina"} />,
+                  <SubCategoryItem subCategoryName={"Gaming"} />
+                ]}
+              />
+              <CategoryItem categoryName={"COMPONENTES"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Procesadores"} />,
+                  <SubCategoryItem subCategoryName={"Tarjetas Madre"} />,
+                  <SubCategoryItem subCategoryName={"RAM"} />,
+                  <SubCategoryItem subCategoryName={"Tarjetas Gráficas"} />,
+                  <SubCategoryItem subCategoryName={"Almacenamiento"} />,
+                  <SubCategoryItem subCategoryName={"Torres"} />,
+                  <SubCategoryItem subCategoryName={"PSUs"} />,
+                  <SubCategoryItem subCategoryName={"Enfriamiento"} />,
+                ]}
+              />
+              <CategoryItem categoryName={"PERIFERICOS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Teclados"} />,
+                  <SubCategoryItem subCategoryName={"Mouse"} />,
+                  <SubCategoryItem subCategoryName={"Headset"} />,
+                  <SubCategoryItem subCategoryName={"Parlantes"} />,
+                  <SubCategoryItem subCategoryName={"Micrófonos"} />,
+                ]}
+              />
+              <CategoryItem categoryName={"CELULARES"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Celulares Nuevos"} />,
+                  <SubCategoryItem subCategoryName={"Celulares Usados"} />,
+                ]}
+              />
+              <CategoryItem categoryName={"TABLETS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Tablets Nuevas"} />,
+                  <SubCategoryItem subCategoryName={"Tablets Usadas"} />,
+                ]}
+              />
+              <CategoryItem categoryName={"ACCESORIOS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Cables"} />,
+                  <SubCategoryItem subCategoryName={"Adaptadores"} />,
+                  <SubCategoryItem subCategoryName={"Extensiones"} />,
+                  <SubCategoryItem subCategoryName={"Redes"} />,
+                ]}
+              />
+              <CategoryItem categoryName={"CONSOLAS"}
+                subCategories={[
+                  <SubCategoryItem subCategoryName={"Consolas Nuevas"} />,
+                  <SubCategoryItem subCategoryName={"Consolas Usadas"} />,
+                ]}
+              />
             </ul>
           </div>
+          {/* Navbar Icons */}
           <div className="navbar-icons mx-5">
-            <div className="w-6 mx-5">
-              <img src="/src/images/search.png" alt=""/>
+            <div className="w-7 mx-4 pt-1 btn-sm btn-ghost btn-square">
+                <img src="/src/images/search.png" alt="search icon"/>
             </div>
-            <div className="w-7 mx-5">
-              <img src="/src/images/shopping-cart.png" alt=""/>
+            <div className="w-9 mx-4 btn-sm btn-ghost btn-square">
+              <img src="/src/images/shopping-cart.png" alt="shopping cart icon"/>
             </div>
-            <div className="w-9 ml-5">
-              <img src="/src/images/user.png" alt="" />
+            <div className="w-9 ml-4 btn-sm btn-ghost btn-circle">
+              <img src="/src/images/user.png" alt="profile icon" />
             </div>
           </div>
         </div>
       </div>
       <div className="drawer-side">
         <label htmlFor="navbar-mobile-menu" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-Footer_color min-h-full w-80 p-4 z-auto">
-          <li><a>item 1</a></li>
-          <li><a>item 2</a></li>
+        <ul className="menu bg-Footer_color min-h-full w-60 p-4">
+          <SidebarCategoryItem categoryName={"LAPTOPS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Laptops Nuevas"}/>,
+              <SubCategoryItem subCategoryName={"Laptops Usadas"}/>
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"COMPUTADORAS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Hogar / Oficina"}/>,
+              <SubCategoryItem subCategoryName={"Gaming"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"COMPONENTES"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Procesadores"}/>,
+              <SubCategoryItem subCategoryName={"Tarjetas Madre"}/>,
+              <SubCategoryItem subCategoryName={"RAM"}/>,
+              <SubCategoryItem subCategoryName={"Tarjetas Gráficas"}/>,
+              <SubCategoryItem subCategoryName={"Almacenamiento"}/>,
+              <SubCategoryItem subCategoryName={"Torres"}/>,
+              <SubCategoryItem subCategoryName={"PSUs"}/>,
+              <SubCategoryItem subCategoryName={"Enfriamiento"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"PERIFÉRICOS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Teclados"}/>,
+              <SubCategoryItem subCategoryName={"Mouse"}/>,
+              <SubCategoryItem subCategoryName={"Headset"}/>,
+              <SubCategoryItem subCategoryName={"Parlantes"}/>,
+              <SubCategoryItem subCategoryName={"Micrófonos"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"CELULARES"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Celulares Nuevos"}/>,
+              <SubCategoryItem subCategoryName={"Celulares Usados"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"TABLETS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Tablets Nuevas"}/>,
+              <SubCategoryItem subCategoryName={"Tablets Usadas"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"ACCESORIOS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Cables"}/>,
+              <SubCategoryItem subCategoryName={"Adaptadores"}/>,
+              <SubCategoryItem subCategoryName={"Accesorios"}/>,
+              <SubCategoryItem subCategoryName={"Redes"}/>,
+            ]}
+          />
+          <SidebarCategoryItem categoryName={"CONSOLAS"}
+            subCategories={[
+              <SubCategoryItem subCategoryName={"Consolas Nuevas"}/>,
+              <SubCategoryItem subCategoryName={"Consolas Usadas"}/>,
+            ]}
+          />
         </ul>
       </div>
     </div>
