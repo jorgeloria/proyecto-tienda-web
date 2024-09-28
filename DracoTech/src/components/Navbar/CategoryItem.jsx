472db859adaa5741454c484804facfc13d0c371a
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-09-28 15:01:08 -0600

    Create category items for the mobile menu

--- DracoTech/src/components/Navbar/CategoryItem.jsx
+++ DracoTech/src/components/Navbar/CategoryItem.jsx
@@ -1,6 +1,6 @@
 const CategoryItem = ({categoryName, subCategories}) => {
   return(
-    <li>
+    <li className="">
     <a className="dropdown dropdown-hover dropdown-bottom">
       <strong className="hover:text-Primary_color">{categoryName}</strong>
       <ul className="dropdown-content menu bg-Footer_color w-52">
