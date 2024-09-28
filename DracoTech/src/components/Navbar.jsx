74719ab7eaf6b06c955eea19ffe78e2398526ad6
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-09-27 18:32:47 -0600

    Create a responsive navbar. Add incomplete subcategories. Add logo. Add icons for search, profile and shopping cart

--- DracoTech/src/components/Navbar.jsx
+++ DracoTech/src/components/Navbar.jsx
@@ -1,135 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/logo_primary_color-transparent.png"
+import "../styles/Navbar.css"
+
 
 const Navbar = () => {
+
+  // Toggle mobile navbar menu
+  const toggleVisibility = () => {
+    let dropdownMenu = document.getElementById("navbar-dropdown-menu")
+    if (dropdownMenu.style.display == "none") {
+      dropdownMenu.style.display = "block";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+  }
+
   return (
-<div className="navbar bg-Footer_color">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+    <div className="drawer">
+      <input id="navbar-mobile-menu" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Navbar */}
+        <div className="navbar flex bg-Footer_color justify-between">
+          {/* Category Button (for mobile) */}
+          <label htmlFor="navbar-mobile-menu" tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-6 w-6 stroke-current">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </label>
+          {/* Navbar Logo */}
+          <div className="avatar w-16 mx-5 rounded">
+              <img src="/src/images/logo_primary_color-transparent.png" alt=""/>
+          </div>
+          {/* Navbar Categories */}
+          <div className="navbar-categories hidden lg:block w-full mx-10">
+            <ul className="menu menu-horizontal flex justify-evenly">
+              <li>
+                <a className="dropdown dropdown-hover dropdown-bottom dropdown-center">
+                  <strong>LAPTOPS</strong>
+                  <ul className="dropdown-content menu bg-Footer_color rounded-box w-52">
+                    <li><a>Laptops Nuevas</a></li>
+                    <li><a>Laptops Usadas</a></li>
+                  </ul>
+                </a>
+              </li>
+              <li><a><strong>COMPUTADORAS</strong></a></li>
+              <li><a><strong>COMPONENTES</strong></a></li>
+              <li><a><strong>PERIFERICOS</strong></a></li>
+              <li><a><strong>CELULARES</strong></a></li>
+              <li><a><strong>TABLETS</strong></a></li>
+              <li><a><strong>ACCESORIOS</strong></a></li>
+              <li><a><strong>CONSOLAS</strong></a></li>
+            </ul>
+          </div>
+          <div className="navbar-icons mx-5">
+            <div className="w-6 mx-5">
+              <img src="/src/images/search.png" alt=""/>
+            </div>
+            <div className="w-7 mx-5">
+              <img src="/src/images/shopping-cart.png" alt=""/>
+            </div>
+            <div className="w-9 ml-5">
+              <img src="/src/images/user.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-Footer_color rounded-box z-[1] mt-3 w-52 p-2 shadow [&>*]:p-1">
-        <li>
-          <details>
-            <summary>LAPTOPS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Laptops Nuevas</a></li>
-              <li><a>Laptops Usadas</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>COMPUTADORAS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Hogar / Oficina</a></li>
-              <li><a>Gaming</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>COMPONENTES</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Procesadores</a></li>
-              <li><a>Tarjetas Madre</a></li>
-              <li><a>RAM</a></li>
-              <li><a>Tarjetas Gráficas</a></li>
-              <li><a>Almacenamiento</a></li>
-              <li><a>Torres</a></li>
-              <li><a>PSUs</a></li>
-              <li><a>Enfriamiento</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>PERIFÉFICOS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Teclados</a></li>
-              <li><a>Mouse</a></li>
-              <li><a>Headset</a></li>
-              <li><a>Parlantes</a></li>
-              <li><a>Micrófonos</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>CELULARES</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Celulares Nuevos</a></li>
-              <li><a>Celulares Usados</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>TABLETS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Tablets Nuevas</a></li>
-              <li><a>Tablets Usadas</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>ACCESORIOS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Cables</a></li>
-              <li><a>Adaptadores</a></li>
-              <li><a>Extensiones</a></li>
-              <li><a>Redes</a></li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <details>
-            <summary>CONSOLAS</summary>
-            <ul className="p-2 [&>*]:p-1">
-              <li><a>Consolas Nuevas</a></li>
-              <li><a>Consolas Usadas </a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
+      <div className="drawer-side">
+        <label htmlFor="navbar-mobile-menu" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu bg-Footer_color min-h-full w-80 p-4 z-auto">
+          <li><a>item 1</a></li>
+          <li><a>item 2</a></li>
+        </ul>
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
   );
 };
 
 export default Navbar;
+
