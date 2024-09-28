2a72f6bbbe56901ec9d8f7a4160efbd96f39993a
Fray <frayaal27@hotmail.com>
2024-09-27 21:54:17 -0600

    Carrusel de tarjetas sin título

--- DracoTech/src/pages/Home.jsx
+++ DracoTech/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import CardCarousel from "../components/CardCarousel";
+
+const CardData = [
+  { name: "Card 1", image: "https://via.placeholder.com/150", price: 100 },
+  { name: "Card 2", image: "https://via.placeholder.com/150", price: 10 },
+  { name: "Card 3", image: "https://via.placeholder.com/150", price: 0 },
+  { name: "Card 4", image: "https://via.placeholder.com/150", price: 1 },
+  { name: "Card 5", image: "https://via.placeholder.com/150", price: 10 },
+  { name: "Card 6", image: "https://via.placeholder.com/150", price: 0 },
+  { name: "Card 9", image: "https://via.placeholder.com/150", price: 1 },
+];
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <CardCarousel CardData={CardData} />
     </div>
   );
 };
