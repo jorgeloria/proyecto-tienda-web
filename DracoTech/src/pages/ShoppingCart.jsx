e88604a80f8c692e57c88f789f7319fcdc5df703
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-10-03 00:30:57 -0600

    Design summary column. Use SummaryItem items

--- DracoTech/src/pages/ShoppingCart.jsx
+++ DracoTech/src/pages/ShoppingCart.jsx
@@ -3,15 +3,16 @@ import "../styles/ShoppingCart.css"
 
 import Monitor from "/src/images/Monitor.png"
 import ShoppingCartItem from '../components/ShoppingCart/ShoppingCartItem';
+import SummaryItem from '../components/ShoppingCart/SummaryItem';
 
 const ShoppingCart = () => {
   return (
   <>
-    <div className="shopping-cart-grid grid md:grid-cols-12 lg:mx-48 mt-20 overflow-x-auto">
+    <div className="shopping-cart-grid grid md:grid-cols-12 2xl:mx-48 lg:mx-16 mt-20 overflow-x-auto">
       <div className="products-section lg:col-span-7 overflow-x-auto">
-        <h1>Mi Carrito</h1>
-        <table className="table text-base">
-          <thead className="invisible md:visible text-base">
+        <h1 className="header text-4xl my-4">Mi Carrito</h1>
+        <table className="table cart-table text-base">
+          <thead className="invisible md:visible text-base text-white">
             <tr>
               <th></th> {/*Product Image*/}
               <th>Producto</th>
@@ -52,35 +53,29 @@ const ShoppingCart = () => {
 
       
 
-      <div className="summary-section md:col-span-5">
-        <h1>Resumen</h1>
-        <table className="table text-base">
-          <thead>
-            <tr>
-              <th></th> {/*Column Name: Subtotal, Envío, IVA, Total*/}
-              <th></th> {/*Column Value: Precio*/}
-            </tr>
-          </thead>
+      <div className="summary-section md:col-span-5 flex grid md:grid-rows-12 mx-10 px-4 bg-Card_color rounded-lg">
+        <h1 className='header row-span-2 text-4xl text-center align-center mt-4'>Resumen</h1>
+        <table className="table summary-table row-span-6 text-base mx-4">
           <tbody>
-            <tr>
-              <td>Subtotal</td>
-              <td>₡ 543,700</td>
-            </tr>
-            <tr>
-              <td>Envío</td>
-              <td>₡ 6,000</td>
-            </tr>
-            <tr>
-              <td>IVA</td>
-              <td>₡ 62,549</td>
-            </tr>
-            <tr>
-              <td>Total</td>
-              <td>₡ 549,700</td>
-            </tr>
+            <SummaryItem
+              name={"Subtotal"}
+              value={"543,700"}
+            />
+            <SummaryItem
+              name={"Envío"}
+              value={"6,000"}
+            />
+            <SummaryItem
+              name={"IVA"}
+              value={"62,549"}
+            />
+            <SummaryItem
+              name={"Total"}
+              value={"549,700"}
+            />
           </tbody>
         </table>
-        <button className="pay-button btn btn-ghost">Pagar</button>
+        <button className="pay-button row-span-4 w-full h-16 text-xl btn btn-ghost hard-center bg-Tertiary_color">Pagar</button>
       </div>
     </div>
   </>);
