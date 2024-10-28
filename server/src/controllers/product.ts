05d659e2a6023788c62ae75fffbafc2c4aed4aeb
Monchez9 <moisesariasg@gmail.com>
2024-10-28 13:11:08 -0600

    clase producto
    para uniformidad de los datos

--- server/src/controllers/product.ts
+++ server/src/controllers/product.ts
@@ -7,6 +7,7 @@ export class ProductController {
 	public async getProducts(req: Request, res: Response) {
 		try {
 			const products = await this.dataAccess.getProducts();
+			console.log(products);
 			return res.status(200).json(products);
 		} catch (error) {
 			return res.status(500).json({
--- server/src/handlers/JSONHandler.ts
+++ server/src/handlers/JSONHandler.ts
@@ -1,18 +1,21 @@
 import { JSONFilesHelper } from "../filesHelpers/JSONFilesHelper";
+import { Product } from "../class/product";
 export class JSONHandler {
 	private jsonFilesHelper = new JSONFilesHelper();
 
 	public async readProducts() {
-		const products = await this.jsonFilesHelper.readJSONFile();
-		return products.products.map((product: any) => ({
-			id: product.id,
-			title: product.title,
-			description: product.description,
-			price: product.price,
-			stock: product.stock,
-			category: product.category,
-			images: product.images,
-			thumbnail: product.thumbnail,
-		}));
+		const productsData  = await this.jsonFilesHelper.readJSONFile();
+		return productsData.products.map((product: any) => {
+			return new Product(
+					product.id,
+					product.title,
+					product.description,
+					product.price,
+					product.stock,
+					product.category,
+					product.images,
+					product.thumbnail
+			);
+		});
 	}
 }
