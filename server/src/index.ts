6b43f1136ad9b511e8d22ed39ef2aaa3a697eb65
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-10-31 16:03:06 -0600

    Use express.json() to handle json requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Proyecto tienda virtual Backend/API-Server");
